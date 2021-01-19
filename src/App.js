52576653a8dfe0cdf370bebb693da0929efe3adf
Nick Damario <nicholasgd@comcast.net>
2021-01-19 15:44:25 -0500

    adding api to app.js (notworking)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+    }
+  }
+
+  componentDidMount() {
+    fetch("https://edamam-recipe-search.p.rapidapi.com/search?q=chicken", {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-key": "5ae314507amsh08d20dc5a100a62p133dc6jsn10814122861f",
+        "x-rapidapi-host": "edamam-recipe-search.p.rapidapi.com"
+      }
+    })
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          hits: json,
+        })
+      })
+      .catch(err => {
+        console.error(err);
+      });
+
+  }
+
+  render() {
+    var { isLoaded, hits } = this.state;
+
+    if (!isLoaded) {
+      return <div>Loading...</div>
+    }
+
+    else {
+      return (
+        <div className="App">
+
+          <ul>
+            { hits.recipe.map(hits => (
+              <li key={hits.recipe.label}>
+                Recipe: {hits.recipe.label} | Calories: {hits.recipe.calories}
+              </li>
+
+            ))}
+          </ul>
+
+        </div>
+      );
+    }
+  }
+
 }
 
 export default App;
