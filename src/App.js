f90345d2b44dcc32be5093af610d37c4c02372e8
Samantha <Samanthafoster17@gmail.com>
2021-01-23 13:21:29 -0500

    recipe images, title, calorie display

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import Signup from './components/Signup';
+// import Signup from './components/Signup';
 import './App.css';
-import SearchResultContainer from './components/SearchResultContainer';
+// import SearchResultContainer from './components/SearchResultContainer';
+import RecipeSearch from './components/RecipeSearch';
 
 
 
 function App(){
-  return <Signup />
+  // return <Signup />
   // return <SearchResultContainer />;
+return (
+  <div className="APP">
+    <RecipeSearch />
+  </div>
+)
 }
 
 export default App;
