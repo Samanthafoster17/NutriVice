a8a0c6037c04506b0e3403b39e3f01ba7e814e27
Samantha <Samanthafoster17@gmail.com>
2021-01-25 18:52:58 -0500

    Sponacular

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@
 import './App.css';
 // import SearchResultContainer from './components/SearchResultContainer';
 // import RecipeSearch from './components/RecipeSearch';
-import BreakfastSearch from "./components/BreakfastSearch";
-import SnackSearch from "./components/SnackSearch";
-import LunchSearch from "./components/LunchSearch";
-import DinnerSearch from "./components/DinnerSearch";
+// import BreakfastSearch from "./components/BreakfastSearch";
+// import SnackSearch from "./components/SnackSearch";
+// import LunchSearch from "./components/LunchSearch";
+// import DinnerSearch from "./components/DinnerSearch";
+import Spoonacular from './components/Spoonacular';
 
 
 function App(){
@@ -13,12 +14,13 @@ function App(){
   // return <SearchResultContainer />;
 return (
   <div className="APP">
+     <Spoonacular />
     {/* <RecipeSearch /> */}
-    <BreakfastSearch />
-    <SnackSearch />
-    <LunchSearch />
-    <SnackSearch />
-    <DinnerSearch />
+    {/* <BreakfastSearch /> */}
+    {/* <SnackSearch /> */}
+    {/* <LunchSearch /> */}
+    {/* <SnackSearch /> */}
+    {/* <DinnerSearch /> */}
   </div>
 )
 }
