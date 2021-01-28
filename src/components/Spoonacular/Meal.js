1197fc1a148c3511ad8a2517cc0d6db66df6d100
Samantha <Samanthafoster17@gmail.com>
2021-01-28 12:46:02 -0500

    user has ability to add activity level, age, gender to determine calorie bracket

--- src/components/Spoonacular/Meal.js
+++ src/components/Spoonacular/Meal.js
@@ -1,35 +1,35 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Meal({ meal }) {
- const [imageUrl, setImageUrl] = useState("");
-
-  
-
- useEffect(() => {
-     fetch(
-         `https://api.spoonacular.com/recipes/${meal.id}/information?apiKey=949cc38773a947e191d05b89a2b7e263&includeNutrition=false`
-     )
-     .then((response) => response.json())
-     .then((data) => {
-         setImageUrl(data.image);
-         console.log(data)
-     })
-     .catch(() => {
-         console.log("error");
-     })
- }, [meal.id])
+    const [imageUrl, setImageUrl] = useState("");
 
 
-    return(
+
+    useEffect(() => {
+        fetch(
+            `https://api.spoonacular.com/recipes/${meal.id}/information?apiKey=949cc38773a947e191d05b89a2b7e263&includeNutrition=false`
+        )
+            .then((response) => response.json())
+            .then((data) => {
+                setImageUrl(data.image);
+                console.log(data)
+            })
+            .catch(() => {
+                console.log("error");
+            })
+    }, [meal.id])
+
+
+    return (
         <article>
-           <h1>{meal.title}</h1>
-           <img src={imageUrl} alt="recipe" />
-           <ul>
-               <li>Preperation time: {meal.readyInMinutes} minutes </li>
-               <li>Number of servings: {meal.servings} </li>
-           </ul>
+            <h1>{meal.title}</h1>
+            <img src={imageUrl} alt="recipe" />
+            <ul>
+                <li>Preperation time: {meal.readyInMinutes} minutes </li>
+                <li>Number of servings: {meal.servings} </li>
+            </ul>
 
-           <a href={meal.sourceUrl}>Go to recipe</a>
+            <a href={meal.sourceUrl}>Go to recipe</a>
         </article>
     )
 }
