7a5e0d3a8cec1bddab7101b81a3aa34a98cccfe9
Samantha <Samanthafoster17@gmail.com>
2021-02-01 01:22:35 -0500

    mobile responsiveness updated

--- client/src/components/Meal/index.js
+++ client/src/components/Meal/index.js
@@ -6,7 +6,7 @@ export default function Meal({ meal }) {
 
     useEffect(() => {
         fetch(
-            `https://api.spoonacular.com/recipes/${meal.id}/information?apiKey=9ea0b3f72d644afe9f02e4073a87573d&includeNutrition=false`
+            `https://api.spoonacular.com/recipes/${meal.id}/information?apiKey=949cc38773a947e191d05b89a2b7e263&includeNutrition=false`
         )
             .then((respone) => respone.json())
             .then((data) => {
@@ -19,24 +19,29 @@ export default function Meal({ meal }) {
 
 
     return (
-
+        <>
         <article>
 
             <div className="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe">
-                <h1>{meal.title}</h1>
-                <img src={imageUrl} alt="recipe" className="img-responsive" />
-                <ul>
-                    <li>Preperation time: {meal.readyInMinutes} minutes </li>
-                    <li>Number of servings: {meal.servings} </li>
-                </ul>
+          <div className="card">
+                <h1 className="card-title">{meal.title}</h1>
+                
+                <img src={imageUrl} alt="recipe" className="card-img-top" />
+                <div className="card-body">
+                <p className="card-text">
+                    Preperation time: {meal.readyInMinutes} minutes <br />
+                    Number of servings: {meal.servings} <br />
+                </p>
 
 
-                <button id="filterBtnTwo" className="btn btn-default filter-button" href={meal.sourceUrl}>Go to recipe</button>
+                
             </div>
-
+            <button id="filterBtnTwo" className="btn btn-default filter-button card-bottom" href={meal.sourceUrl}>Go to recipe</button>
+            </div>
+            </div>
         </article>
 
-
+     </>
 
     )
 }
--- client/src/components/Signup/index.js
+++ client/src/components/Signup/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React, { Component} from 'react';
 import './style.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 class  Signup extends Component{
