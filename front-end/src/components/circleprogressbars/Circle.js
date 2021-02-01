9f7f31f4af6e7f3918c42d7617c56e51c06ea179
lucasBRYG <adio.lucas@gmail.com>
2021-02-01 10:18:10 -0500

    circle progress bar

--- front-end/src/components/circleprogressbars/Circle.js
+++ front-end/src/components/circleprogressbars/Circle.js
@@ -5,7 +5,7 @@ import CircleProgress from "@material-ui/core/CircularProgress";
 import "./Circle.css"
 export default function Circle() {
 
-    const [progress, setProgress] = useState(45);
+    const [progress, setProgress] = useState(79);
 
     return (
         <div className = "circle">
