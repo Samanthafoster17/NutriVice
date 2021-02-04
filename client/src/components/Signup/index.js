85608611e813b0c3f905c8499a6f5ed58bc0c623
lucasBRYG <adio.lucas@gmail.com>
2021-02-03 20:11:03 -0500

    finsished signup page styling

--- client/src/components/Signup/index.js
+++ client/src/components/Signup/index.js
@@ -67,80 +67,79 @@ class  Signup extends Component{
 render() {
    const { errors } = this.state;
    return (
-      <div className="container-fluid main">
-          <div className="container-fluid">
-              <h1 className="lead brand">
+      <div className="container-fluid signup-container">
+             <div className = "pillar">
+               <h1 className="lead brand">
                   NutriVice
-              </h1>
-              <h3 className="quotes"> Famous quote</h3>
-              <h4 className=" quotes"> “Health requires healthy food.”</h4>
-                <h6 className=" quotes"> – Roger Williams</h6>
+               </h1>
+               <div className = "quote">
+                  <h4 className=" quotes"> “Health requires healthy food.”</h4>
+                  <h6 className=" quotes"> – Roger Williams</h6>      
+               </div>
+                  {/* <h3 className="quotes"> Famous quote</h3> */}
                
-               {/* user signin form */}
-              <form className="signup" noValidate onSubmit={this.onSubmit}>
-              <div className="mb-3">
-               <label htmlFor="name" className="form-label">Name</label>
-               <input type="text" className="form-control form-group"  
-               onChange={this.onChange}
-               value={this.state.name}
-               error={errors.name}
-               id="name"
-               type="text"
-               />
+                  {/* user signin form */}
+                  <form className="signup" noValidate onSubmit={this.onSubmit}>
+                  <div className="mb-3">
+                     <label htmlFor="name" className="form-label">Name</label>
+                     <input type="text" className="form-control form-group"  
+                     onChange={this.onChange}
+                     value={this.state.name}
+                     error={errors.name}
+                     id="name"
+                     type="text"
+                     />
+                  </div>
+               <div className="mb-3">
+                  <label htmlFor="username" className="form-label">Username</label>
+                  <input type="tel" className="form-control form-group"  
+                  onChange={this.onChange}
+                  value={this.state.username}
+                  error={errors.username}
+                  id="username"
+                  type="text"
+                  />
+               </div>
+               <div className="mb-3">
+                  <label htmlFor="email" className="form-label">Email</label>
+                  <input type="email" className="form-control form-group" 
+                  onChange={this.onChange}
+                  value={this.state.email}
+                  error={errors.email}
+                  id="email"
+                  type="text"
+                  />
+               </div>
+               <div className="mb-3">
+                  <label htmlFor="password" className="form-label">Password</label>
+                  <input type="password" className="form-control form-group"
+                  onChange={this.onChange}
+                  value={this.state.password}
+                  error={errors.password}
+                  id="password"
+                  type="password"
+                  />
+               </div>
+               <div className="mb-3">
+                  <label htmlFor="password" className="form-label">Confirm Password</label>
+                  <input type="password" className="form-control form-group"
+                  onChange={this.onChange}
+                  value={this.state.password2}
+                  error={errors.password2}
+                  id="password2"
+                  type="password"
+                  />
+               </div>
+               <button type="submit" className="btn btn-primary">Register</button>
+               <p className="register grey-text text-darken-1">
+                  Don't have an account? <a href="/login">Login</a>
+               </p>
+            </form>
             </div>
-            <div className="mb-3">
-               <label htmlFor="username" className="form-label">Username</label>
-               <input type="tel" className="form-control form-group"  
-                onChange={this.onChange}
-                value={this.state.username}
-                error={errors.username}
-                id="username"
-                type="text"
-               />
-            </div>
-            <div className="mb-3">
-               <label htmlFor="email" className="form-label">Email</label>
-               <input type="email" className="form-control form-group" 
-                onChange={this.onChange}
-                value={this.state.email}
-                error={errors.email}
-                id="email"
-                type="text"
-               />
-            </div>
-            <div className="mb-3">
-               <label htmlFor="password" className="form-label">Password</label>
-               <input type="password" className="form-control form-group"
-                onChange={this.onChange}
-                value={this.state.password}
-                error={errors.password}
-                id="password"
-                type="password"
-               />
-            </div>
-            <div className="mb-3">
-               <label htmlFor="password" className="form-label">Confirm Password</label>
-               <input type="password" className="form-control form-group"
-                onChange={this.onChange}
-                value={this.state.password2}
-                error={errors.password2}
-                id="password2"
-                type="password"
-               />
-            </div>
-            <button type="submit" className="btn btn-primary">Register</button>
-            <p className="grey-text text-darken-1">
-                Don't have an account? <a href="/login">Login</a>
-              </p>
-          </form>
+               
           {/* sales texts */}
-          </div>
-           <div>
-          
-           </div>
-
       </div>
-    )
+   )
 }
 }
 
