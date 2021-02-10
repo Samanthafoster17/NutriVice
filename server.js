664e9d85654d89bf115b13c2def1e05937a64ee6
Nick Damario <nicholasgd@comcast.net>
2021-02-10 18:12:09 -0500

    deploying

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const passport = require('passport');
 const app = express();
 const secretOrKey = "secret";
-const cors =require("cors");
+const cors = require("cors");
 require('dotenv').config()
 console.log(process.env)
 
@@ -17,26 +17,23 @@ app.use(
 );
 app.use(bodyParser.json());
 
-
 const PORT = process.env.PORT || 5000;
 // connect to database
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/nutrivicedb",
-  {useNewUrlParser: true,
-    useUnifiedTopology: true
-  }) 
-.then(() => console.log("Successful connection to mongoDB"))
-.catch( err => console.log(err));
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log("Successful connection to mongoDB"))
+  .catch(err => console.log(err));
 
-
-mongoose.connection.on('connected', () =>{
-  console.log('Mongoose is Connected!!!')
-})
-
-if(process.env.NODE_ENV !== "production"){
+if (process.env.NODE_ENV !== "production") {
   app.options("*", cors());
   app.use(cors());
-  app.use(express.static('client/build'))
-} 
+
+}
 
 app.use(express.json());
 
@@ -46,6 +43,6 @@ require('./config/passport')(passport);
 
 app.use(require('./routes'));
 
-app.listen(PORT, function() {
-    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+app.listen(PORT, function () {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
