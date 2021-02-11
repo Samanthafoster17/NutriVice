f47f159a4c8203d9ce78d822eb50d99f4004ba6b
Nick Damario <nicholasgd@comcast.net>
2021-02-11 16:46:12 -0500

    changing discover route name

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import GetMeal from './components/GetMealData';
 import DietPref from "./components/DietPref";
 import Chart from "./components/Chart";
 import LandingPage from "./pages/landingpage/LandingPage";
-import dashboard from "./pages/Dashboard";
+import Dashboard from "./pages/Dashboard";
 import Profile from "./components/Profile";
 import Discover from "./pages/Discover";
 import About from "./pages/About";
