2567bcb4f8cb4986ee88d7510a7e3a6b59543418
Samantha <Samanthafoster17@gmail.com>
2021-04-19 23:05:33 -0400

    correcting register

--- client/src/components/Dashboard/index.js
+++ client/src/components/Dashboard/index.js
@@ -38,7 +38,7 @@ const Dashboard1 = (props) => {
             console.log("welcome");
         } else if (!userInfo) {
             // setAuthenticated(false);
-            // props.history.push("/Signin");
+            props.history.push("/signin");
             window.location.assign('/signin')
         }
         console.log(userInfo);
