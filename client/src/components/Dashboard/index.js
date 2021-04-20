a18f83d34f5c0a197d7a0526a36299be338d0921
Samantha <Samanthafoster17@gmail.com>
2021-04-19 22:46:23 -0400

    correcting register

--- client/src/components/Dashboard/index.js
+++ client/src/components/Dashboard/index.js
@@ -34,11 +34,11 @@ const Dashboard1 = (props) => {
         const userInfo = localStorage.getItem("decodedTokenID");
         const regToken = localStorage.getItem("regTokenID");
         if (userInfo || regToken) {
-            // setAuthenticated(true);
+            setAuthenticated(true);
             console.log("welcome");
         } else if (!userInfo) {
-            // setAuthenticated(false);
-            // props.history.push("/Signin");
+            setAuthenticated(false);
+            props.history.push("/Signin");
             window.location.assign('/signin')
         }
         console.log(userInfo);
